cda11ba75c18b8efb6c99c6f225c56377cd12861
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 12:23:08 +0700

    ganti looping pembuatan thread

--- soal5.c
+++ soal5.c
@@ -7,6 +7,7 @@
 void *count(void * arg){
 	char *key=(void *)arg;
 	char command[1000]="";
+	printf("%s : ",key);
 	strcat(command,"grep -o '");
 	strcat(command,key);
 	strcat(command,"' Novel.txt | wc -l");
@@ -16,11 +17,12 @@ void *count(void * arg){
 
 int main(int argc, char **argv){
 	pthread_t tid[argc-1];
+	int i;
 
-	while(--argc){
-		if(pthread_create(&(tid[argc]),NULL,&count,argv[argc])!=0){
+	for(i=1;i<argc;i++){
+		if(pthread_create(&(tid[i]),NULL,&count,argv[i])!=0){
 			exit(EXIT_FAILURE);
 		}
-		pthread_join(tid[argc],NULL);
+		pthread_join(tid[i],NULL);
 	}
 }
