648b4b0893155966cde955f870796342268cecb9
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 16:40:47 +0700

    [+]Fungsi Interface

---
+++ soal3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void hello(){
+	printf("Selamat datang di Game Aidil cari ikan\n");
+	printf("Kami sediakan ikan Lohan dan Kepiting\n");
+	printf("Start (Press any key)\n");
+	system("pause");
+}
+
+int main(){
+
+}
