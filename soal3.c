7c3a4c6cef040b548f17258dee1400b9795dd1ca
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 16:54:56 +0700

    [+]Global Variabel, thread

--- soal3.c
+++ soal3.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <pthread.h>
 
+int Lohanstat=100,
+	Kepitingstat=100,
+	Makan=10;
+
 void hello(){
 	printf("Selamat datang di Game Aidil cari ikan\n");
 	printf("Kami sediakan ikan Lohan dan Kepiting\n");
@@ -11,6 +15,17 @@ void hello(){
 	system("pause");
 }
 
+void *LohanLapar(){
+	sleep(10);
+	Lohanstat-=15;
+}
+
+void *KepitingLapar(){
+	sleep(20);
+	Kepitingstat-=10;
+}
+
 int main(){
+	hello();
 
 }
