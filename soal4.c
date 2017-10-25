e9651d0486feae0e77ef1245bdc925eee551ea01
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 13:43:39 +0700

    #FINISH#

--- soal4.c
+++ soal4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 void swap(int *xp, int *yp){
     int temp = *xp;
@@ -16,7 +18,16 @@ void bubbleSort(int arr[], int n){
               swap(&arr[j], &arr[j+1]);
 }
 
+void *fact(void *arg){
+	int num=*((int *)arg);
+	int i,con=1;
+	for(i=1;i<=num;i++) con*=i;
+	printf("Hasil %d! = %d\n",num,con );
+	sleep(1);
+}
+
 int main(int argc, char **argv){
+	pthread_t tid[argc-1];
 	int x=argc;
 	int data[x-1];
 	for(x=0;x<argc-1;x++){
@@ -24,5 +35,12 @@ int main(int argc, char **argv){
 		strcpy(temp,argv[x+1]);
 		data[x]=atoi(temp);
 	}
-	for(x=0;x<argc-1;x++) printf("%s %d\n",argv[x+1],data[x]);
+	bubbleSort(data,x);
+	for(x=0;x<argc-1;x++){
+			if(pthread_create(&(tid[x]),NULL,&fact,&data[x])!=0){
+				exit(EXIT_FAILURE);
+			}
+			pthread_join(tid[x],NULL);
+		}
+	return 0;
 }
