08d49c30e2e5d2993f1b0777dbe77888827361b7
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 12:19:22 +0700

    Nambah main

--- soal5.c
+++ soal5.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<unistd.h>
+#include<pthread.h>
 
 void *count(void * arg){
-	char key[100]=(void *)arg;
+	char *key=(void *)arg;
 	char command[1000]="";
 	strcat(command,"grep -o '");
 	strcat(command,key);
 	strcat(command,"' Novel.txt | wc -l");
 	system(command);
+	sleep(1);
+}
+
+int main(int argc, char **argv){
+	pthread_t tid[argc-1];
+
+	while(--argc){
+		if(pthread_create(&(tid[argc]),NULL,&count,argv[argc])!=0){
+			exit(EXIT_FAILURE);
+		}
+		pthread_join(tid[argc],NULL);
+	}
 }
