6c93e6067c43a7e9aa3a4f3b8a4fc76792a4bd07
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 17:24:05 +0700

    [+]Fungsi lah

--- soal3.c
+++ soal3.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <pthread.h>
 
 int Lohanstat=100,
@@ -16,33 +17,62 @@ void hello(){
 }
 
 void *LohanLapar(){
-	sleep(10);
-	Lohanstat-=15;
+	while(1){
+		sleep(10);
+		Lohanstat-=15;
+	}
 }
 
 void *KepitingLapar(){
-	sleep(20);
-	Kepitingstat-=10;
+	while(1){
+		sleep(20);
+		Kepitingstat-=10;
+	}
 }
 
-void *Makanin(void * arg){
-	int num=*((int *)arg);
+void *Makanin(){
+	printf("Beri makan :\n");
+	printf("1. Untuk Lohan\n");
+	printf("2. Untuk Kepiting\n");		
+	int num;
+	scanf("%d",&num);
+
 	if(num==1) Lohanstat+=10;
 	if(num==2) Kepitingstat+=10;
 }
 
+void view(){
+	time_t now;
+	now=time(NULL);
+	printf("Status Peliharaan Aidil pada %s\n", ctime(&now));
+	printf("Status Lohan saat ini %d\n",Lohanstat);
+	printf("Status Kepiting saat ini %d\n",Kepitingstat );
+}
+
 int main(){
+	pthread_t tid[3];
 	hello();
-	if
+	if(pthread_create(&(tid[1]),NULL,&LohanLapar,NULL)!=0){
+		exit(EXIT_FAILURE);
+	}
+	if(pthread_create(&(tid[2]),NULL,&KepitingLapar,NULL)!=0){
+		exit(EXIT_FAILURE);
+	}
 	while(1){
+		system("clear");
+		view();
 		if(Lohanstat<=0 || 
 			Lohanstat >100 || 
 			Kepitingstat<=0 || 
 			Kepitingstat>100){
-		system(clear);
+		system("clear");
 		printf("Anda sudah kalah :(\n");
 		break;
 		}
 
+		if(pthread_create(&(tid[0]),NULL,&Makanin,NULL)!=0){
+			exit(EXIT_FAILURE);
+		}
+		sleep(1);
 	}
 }
