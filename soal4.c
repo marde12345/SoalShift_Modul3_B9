412c5a0853645bcc169d39fb779018fedc691b34
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 12:52:18 +0700

    [+]Fungsi BubbleSort

--- soal4.c
+++ soal4.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 void swap(int *xp, int *yp){
     int temp = *xp;
@@ -6,6 +7,21 @@ void swap(int *xp, int *yp){
     *yp = temp;
 }
 
-int main(int argc, char **argv){
+void bubbleSort(int arr[], int n){
+   int i, j;
+   for (i = 0; i < n-1; i++)       
+       for (j = 0; j < n-i-1; j++) 
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
 
+int main(int argc, char **argv){
+	int x=argc-1;
+	int data[x];
+	// while(x--){
+	// 	char temp[256];
+	// 	strcpy(temp,argv[x]);
+	// 	data[x]=atoi(temp)-1;
+	// }
+	// for(x=0;x<argc-1;x++) printf("%d\n",data[x]);
 }
