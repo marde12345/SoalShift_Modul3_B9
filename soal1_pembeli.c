7e5eabbe409f66475197893f5ab5bc079731f98c
Marde Fasma <mardefasma123up@gmail.com>
2017-10-25 15:54:46 +0700

    [+]Main Interface, counter

--- soal1_pembeli.c
+++ soal1_pembeli.c
@@ -1,22 +1,69 @@
 #include <sys/ipc.h>
+#include <stdio.h>
 #include <sys/shm.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-typedef struct weapon{
-	char name[6][7];
-	strcpy(name[0],"MP4A1");
-	strcpy(name[1],"PM2-V1");
-	strcpy(name[2],"SPR-3");
-	strcpy(name[3],"SS2-V5");
-	strcpy(name[4],"SPG1-V3");
-	strcpy(name[5],"MINE");
-	int count[6];
-} weapon_entry;
+int counter(char *weapon){
+	if(!strcmp(weapon,"MP4A1")) return 0;
+	if(!strcmp(weapon,"PM2-V1")) return 1;
+	if(!strcmp(weapon,"SPR-3")) return 2;
+	if(!strcmp(weapon,"SS2-V5")) return 3;
+	if(!strcmp(weapon,"SPG1-V3")) return 4;
+	if(!strcmp(weapon,"Mine")) return 5;
+}
 
+void hello(){
+	printf("Hello, selamat datang di sistem informasi toko ilegal HAWARI\n");
+	printf("Disini kami menyediakan beberapa pelayanan pembelian senjata ilegal\n");
+	printf("Kami juga termasuk Black-Market, jadi identitas anda selalu menjadi Anonymous\n\n");
+	printf("1. List Senjata Yang Tersedia\n");
+	printf("2. Pembelian Senjata\n");
+	printf("3. Exit\n\n");
+	printf("Masukkan pilihan anda : ");
+}
+
+void list(){
+
+}
 int main(){
-	key_t key = 1123458;
-	struct weapon_entry *supply;
-	int shmid = shmget(key,sizeof(weapon_entry), IPC_CREAT | 0666);
-	supply=(weapon_entry *) shmat(shmid,NULL,0);
+	int supply[6];
+	key_t key = 112358;
+	int **data;
+
+	int shmid=shmget(key,6*sizeof(int), IPC_CREAT | 0666);
+	*data = shmat(shmid,NULL,0);
+
+	while(1){
+		int input;
+		system("clear");
+		hello();
+		scanf("%d",&input);
+		switch (input){
+			case 1 :{
+				list(data[]);
+				break;
+			}
+			case 2 :{
+				beli();
+				break;
+			}
+			case 3 :{
+				break;
+			}
+			default :{
+				continue;
+			}
+		}
+	}
+
+	system("clear");
+	printf("Terima Kasih\n");
+	int cd=5;
+	while(cd--){
+		printf(".\n");
+		sleep(1);
+	}
 	return 0;
 }
