5f08e4418e8c2703df13853ff7222c77cf2b32db
Marde Fasma <mardefasma123up@gmail.com>
2017-10-26 21:55:31 +0700

    [+]compare passing fungsi kula'an

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -11,22 +11,53 @@ typedef struct Weapon{
 	int stock;
 }wweapon;
 
-struct Weapon weap[6];
-struct Weapon *p_weap;
+void list(struct Weapon weap[],int p_shmid){
+	system("clear");
+	printf("Persediaan senjata black-market HAWARI\n\n");
+	printf("Connected to SHMID : %d\n", p_shmid);
+	printf("**************************************\n\n");
+	if(weap[0].stock>0)printf("%s %d\n",weap[0].name,weap[0].stock);
+	if(weap[1].stock>0)printf("%s %d\n",weap[1].name,weap[1].stock);
+	if(weap[2].stock>0)printf("%s %d\n",weap[2].name,weap[2].stock);
+	if(weap[3].stock>0)printf("%s %d\n",weap[3].name,weap[3].stock);
+	if(weap[4].stock>0)printf("%s %d\n",weap[4].name,weap[4].stock);
+	if(weap[5].stock>0)printf("%s %d\n",weap[5].name,weap[5].stock);
+}
 
-void init(){
-	strcpy(weap[0].name,"MP4A1");
-	strcpy(weap[1].name,"PM2-V1");
-	strcpy(weap[2].name,"SPR-3");
-	strcpy(weap[3].name,"SS2-V5");
-	strcpy(weap[4].name,"SPG1-V3");
-	strcpy(weap[5].name,"MINE");
+void tambah(struct Weapon weap[],int p_shmid){
+	system("clear");
+	printf("Kula'an senjata black-market HAWARI\n\n");
+	printf("Connected to SHMID : %d\n", p_shmid);
+	printf("**************************************\n");
+	printf("Ex. --> MP4A1 100\n\n--> ");
+	char stemp[8];
+	int temp;
+	scanf("%s%d",stemp,&temp);
+
+	if(strcmp(weap[0].name,stemp)==0)weap[0].stock+=temp;
+	if(strcmp(weap[1].name,stemp)==0)weap[1].stock+=temp;
+	if(strcmp(weap[2].name,stemp)==0)weap[2].stock+=temp;
+	if(strcmp(weap[3].name,stemp)==0)weap[3].stock+=temp;
+	if(strcmp(weap[4].name,stemp)==0)weap[4].stock+=temp;
+	if(strcmp(weap[5].name,stemp)==0)weap[5].stock+=temp;
+}
+
+void init(struct Weapon weap[]){
+	strcpy(weap[0].name,"MP4A1");weap[0].stock=10;
+	strcpy(weap[1].name,"PM2-V1");weap[1].stock=10;
+	strcpy(weap[2].name,"SPR-3");weap[2].stock=10;
+	strcpy(weap[3].name,"SS2-V5");weap[3].stock=10;
+	strcpy(weap[4].name,"SPG1-V3");weap[4].stock=10;
+	strcpy(weap[5].name,"MINE");weap[5].stock=10;
 }
 
 int main(){
 	//obtain access
 	key_t key=112358;
 	int shmid;
+
+	struct Weapon weap[6];
+	struct Weapon *p_weap;
 
 	p_weap = &weap[0];
 
@@ -41,7 +72,7 @@ int main(){
 		exit(1);
 	}
 
-	init();
+	init(p_weap);
 
 	
 
